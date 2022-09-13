bd07b01ffe44682f1967e78cc5724eabb9cabb74
unknown <dggalanti@hotmail.com>
2022-09-13 16:35:47 +0200

    added images to the dice app

--- Dado/app/src/main/java/com/gallardo/dado/MainActivity.kt
+++ Dado/app/src/main/java/com/gallardo/dado/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gallardo.dado
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 /**
@@ -15,18 +16,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { jogarDado() }
+        jogarDado()
     }
 
     /**
      * Roll the dice and update the screen with the result.
      */
     private fun jogarDado() {
-        // Create new Dice object with 6 sides and roll it
+        // Create new Dice object with 6 sides and roll the dice
         val dado = Dado(6)
         val jogada = dado.jogarDado()
 
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = jogada.toString()
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (jogada) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = jogada.toString()
     }
 }
