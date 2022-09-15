70408e2f33a15163f2a280349d2055cfd9ba4a37
unknown <dggalanti@hotmail.com>
2022-09-15 16:20:51 +0200

    Implementation of basic Material Design to improve the app UX

--- TipTime/app/src/main/java/com/gallardo/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/gallardo/tiptime/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.gallardo.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import com.gallardo.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import kotlin.math.ceil
@@ -15,10 +20,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener {view, keyCode, _ -> handleKeyEvent(view, keyCode)}
     }
 
     private fun calculateTip() {
-        val cost = binding.costOfService.text.toString().toDoubleOrNull()
+        val cost = binding.costOfServiceEditText.text.toString().toDoubleOrNull()
         if (cost == null || cost == 0.0) {
             binding.tipResult.text = "0.0"
             return
@@ -33,5 +39,16 @@ class MainActivity : AppCompatActivity() {
                 ceil(cost * tipPercentage)
             else
                 cost * tipPercentage))
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
