1882c1ee090b15a9c48adc2a59a24c4eab8706fd
diegogalanti <dggalanti@hotmail.com>
2022-09-22 09:29:22 +0200

    solved test bug

--- Cupcake/app/src/androidTest/java/com/example/cupcake/ViewModelTests.kt
+++ Cupcake/app/src/androidTest/java/com/example/cupcake/ViewModelTests.kt
@@ -1,16 +1,29 @@
 package com.example.cupcake
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.cupcake.model.OrderViewModel
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 
 class ViewModelTests {
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
     @Test
-    fun quantity_twelve_cupcakes() {
+    fun testquantity_twelve_cupcakes() {
         val viewModel = OrderViewModel()
         viewModel.ccOrderQuantity.observeForever {}
         viewModel.setQuantity(12)
         assertEquals(12, viewModel.ccOrderQuantity.value)
+    }
+
+    @Test
+    fun testprice_twelve_cupcakes() {
+        val viewModel = OrderViewModel()
+        viewModel.setQuantity(12)
+        viewModel.ccTotalPrice.observeForever {}
+        assertEquals("$27.00", viewModel.ccTotalPrice.value)
     }
 }
