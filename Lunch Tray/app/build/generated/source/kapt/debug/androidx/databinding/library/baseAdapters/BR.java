e70088799118eb27491dbf9acc3f17468f862447
diegogalanti <dggalanti@hotmail.com>
2022-09-23 18:37:58 +0200

    Complete Luch Tray app passing all tests

---
+++ Lunch Tray/app/build/generated/source/kapt/debug/androidx/databinding/library/baseAdapters/BR.java
@@ -0,0 +1,15 @@
+package androidx.databinding.library.baseAdapters;
+
+public class BR {
+  public static final int _all = 0;
+
+  public static final int entreeFragment = 1;
+
+  public static final int fragmentAccompaniment = 2;
+
+  public static final int fragmentCheckout = 3;
+
+  public static final int sideFragment = 4;
+
+  public static final int viewModel = 5;
+}
---
+++ Lunch Tray/app/build/generated/source/kapt/debug/com/example/lunchtray/BR.java
@@ -0,0 +1,15 @@
+package com.example.lunchtray;
+
+public class BR {
+  public static final int _all = 0;
+
+  public static final int entreeFragment = 1;
+
+  public static final int fragmentAccompaniment = 2;
+
+  public static final int fragmentCheckout = 3;
+
+  public static final int sideFragment = 4;
+
+  public static final int viewModel = 5;
+}
