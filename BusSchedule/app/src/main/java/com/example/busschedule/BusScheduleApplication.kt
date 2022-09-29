abcf2a17de0eacecf9661bb518fc4626f00f114e
diegogalanti <dggalanti@hotmail.com>
2022-09-29 17:44:11 +0200

    Adjust BusSchedule app using data binding like in Amphibians app

--- BusSchedule/app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
+++ BusSchedule/app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
@@ -1,4 +1,8 @@
 package com.example.busschedule
 
-class BusScheduleApplication {
+import android.app.Application
+import com.example.busschedule.database.schedule.AppDatabase
+
+class BusScheduleApplication : Application() {
+    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
 }
--- BusSchedule/app/src/main/java/com/example/busschedule/BusStopAdapter.kt
+++ BusSchedule/app/src/main/java/com/example/busschedule/BusStopAdapter.kt
@@ -1,4 +1,65 @@
 package com.example.busschedule
 
-class BusStopAdapter {
+import android.annotation.SuppressLint
+import android.icu.text.SimpleDateFormat
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.busschedule.database.schedule.Schedule
+import com.example.busschedule.databinding.BusStopItemBinding
+import java.util.*
+
+class BusStopAdapter(val clickListener: ScheduleListener) : ListAdapter<Schedule, BusStopAdapter.BusStopViewHolder>(DiffCallback)
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BusStopViewHolder
+    {
+        val viewHolder = BusStopViewHolder(
+            BusStopItemBinding.inflate(
+                LayoutInflater.from( parent.context),
+                parent,
+                false
+            )
+        )
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: BusStopViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(getItem(position), clickListener)
+    }
+
+    class BusStopViewHolder(private var binding: BusStopItemBinding): RecyclerView.ViewHolder(binding.root)
+    {
+        @SuppressLint("SimpleDateFormat")
+        fun bind(schedule: Schedule, scheduleListener: ScheduleListener)
+        {
+            binding.arrivalTimeTextView.text = SimpleDateFormat(
+                "h:mm a").format(
+                Date(schedule.arrivalTime.toLong() * 1000)
+            )
+            binding.scheduleListener = scheduleListener
+            binding.schedule = schedule
+        }
+    }
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<Schedule>()
+        {
+            override fun areItemsTheSame(oldItem: Schedule, newItem: Schedule): Boolean
+            {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Schedule, newItem: Schedule): Boolean
+            {
+                return oldItem == newItem
+            }
+        }
+    }
+}
+
+class ScheduleListener(val clickListener: (schedule: Schedule) -> Unit) {
+    fun onClick(schedule: Schedule) = clickListener(schedule)
 }
