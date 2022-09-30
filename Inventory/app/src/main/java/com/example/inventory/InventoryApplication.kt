37c5534f3b06962016960ff6a7009531ef804682
diegogalanti <dggalanti@hotmail.com>
2022-09-30 13:11:25 +0200

    Crested the Room Database class and initiated it in the Application with 'by lazy'

--- Inventory/app/src/main/java/com/example/inventory/InventoryApplication.kt
+++ Inventory/app/src/main/java/com/example/inventory/InventoryApplication.kt
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.inventory
-
 import android.app.Application
 
-class InventoryApplication : Application()
+class InventoryApplication : Application(){
+    val database: ItemRoomDatabase by lazy { ItemRoomDatabase.getDatabase(this) }
+}
