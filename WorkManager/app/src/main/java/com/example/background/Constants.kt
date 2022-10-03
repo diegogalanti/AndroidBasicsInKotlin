df24bfaf6665f2986e26f58cea731fb1aab9f4e4
diegogalanti <dggalanti@hotmail.com>
2022-10-03 20:28:03 +0200

    Implemented a Worker chain with WorkManager

--- WorkManager/app/src/main/java/com/example/background/Constants.kt
+++ WorkManager/app/src/main/java/com/example/background/Constants.kt
@@ -35,6 +35,6 @@ const val IMAGE_MANIPULATION_WORK_NAME = "image_manipulation_work"
 // Other keys
 const val OUTPUT_PATH = "blur_filter_outputs"
 const val KEY_IMAGE_URI = "KEY_IMAGE_URI"
-const val TAG_OUTPUT = "OUTPUT"
+const val TAG_OUTPUT = "OUTPUT2"
 
 const val DELAY_TIME_MILLIS: Long = 3000
